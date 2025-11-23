f135cf6881c8b5a78b7941179dbb146fdff22266
James Woods <jmwoodsjr@gmail.com>
2025-11-22 21:23:16 -0500

    Refactored first if statment comparing item.Name not equal aged brie and not equal backstage passes into two separate ifs

--- csharp.xUnit_3/GildedRose/GildedRose.cs
+++ csharp.xUnit_3/GildedRose/GildedRose.cs
@@ -15,13 +15,42 @@ public class GildedRose
     {
         foreach (var item in Items)
         {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name != "Aged Brie")
             {
-                if (item.Quality > 0)
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    if (item.Quality > 0)
                     {
-                        item.Quality -= 1;
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality -= 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality += 1;
+
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (item.SellIn < 11)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality += 1;
+                                }
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality += 1;
+                                }
+                            }
+                        }
                     }
                 }
             }
