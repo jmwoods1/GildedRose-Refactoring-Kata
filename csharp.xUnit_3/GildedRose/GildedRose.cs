510913e136f7574d8165e2ebcb1715e1a576609d
James Woods <jmwoodsjr@gmail.com>
2025-11-22 22:06:08 -0500

    Removed braces on else statement after if check on sulfuras to change to else if check on backstage passes

--- csharp.xUnit_3/GildedRose/GildedRose.cs
+++ csharp.xUnit_3/GildedRose/GildedRose.cs
@@ -18,48 +18,21 @@ public class GildedRose
             if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
             }
-            else
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality < 50)
                 {
-                    if (item.Quality < 50)
+                    item.Quality += 1;
+
+                    if (item.SellIn < 11)
                     {
-                        item.Quality += 1;
-
-                        if (item.SellIn < 11)
+                        if (item.Quality < 50)
                         {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
+                            item.Quality += 1;
                         }
                     }
 
-                    item.SellIn -= 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        item.Quality -= item.Quality;
-                    }
-                }
-                else if (item.Name == "Aged Brie")
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
-
-                    item.SellIn -= 1;
-
-                    if (item.SellIn < 0)
+                    if (item.SellIn < 6)
                     {
                         if (item.Quality < 50)
                         {
@@ -67,21 +40,45 @@ public class GildedRose
                         }
                     }
                 }
-                else
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
+                {
+                    item.Quality -= item.Quality;
+                }
+            }
+            else if (item.Name == "Aged Brie")
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+                }
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality += 1;
+                    }
+                }
+            }
+            else
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality -= 1;
+                }
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
                 {
                     if (item.Quality > 0)
                     {
                         item.Quality -= 1;
-                    }
-
-                    item.SellIn -= 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality -= 1;
-                        }
                     }
                 }
             }
