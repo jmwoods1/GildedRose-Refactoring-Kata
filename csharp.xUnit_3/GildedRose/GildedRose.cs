7ed47a9fc27a1a6cb80e352fa232d68375b4dc49
James Woods <jmwoodsjr@gmail.com>
2025-11-22 21:52:30 -0500

    Removed all unreachable code

--- csharp.xUnit_3/GildedRose/GildedRose.cs
+++ csharp.xUnit_3/GildedRose/GildedRose.cs
@@ -18,92 +18,40 @@ public class GildedRose
             var isBackstagePasses = item.Name == "Backstage passes to a TAFKAL80ETC concert";
             if (isBackstagePasses)
             {
-                if (item.Name == "Aged Brie")
+                if (true)
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality += 1;
-                    }
 
-                    item.SellIn -= 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality < 50)
+                        if (true)
                         {
-                            item.Quality += 1;
+                            if (item.SellIn < 11)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality += 1;
+                                }
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality += 1;
+                                }
+                            }
                         }
                     }
                 }
-                else
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
                 {
                     if (true)
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
-
-                            if (true)
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name == "Sulfuras, Hand of Ragnaros")
-                            {
-                            }
-                            else
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
-                    }
-
-                    if (item.Name == "Sulfuras, Hand of Ragnaros")
-                    {
-                    }
-                    else
-                    {
-                        item.SellIn -= 1;
-                    }
-
-                    if (item.SellIn < 0)
-                    {
-                        if (true)
-                        {
-                            item.Quality -= item.Quality;
-                        }
-                        else
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                                {
-                                }
-                                else
-                                {
-                                    item.Quality -= 1;
-                                }
-                            }
-                        }
+                        item.Quality -= item.Quality;
                     }
                 }
             }
@@ -128,43 +76,14 @@ public class GildedRose
                 }
                 else
                 {
-                    if (false)
+                    if (item.Quality > 0)
                     {
-                        if (item.Quality < 50)
+                        if (item.Name == "Sulfuras, Hand of Ragnaros")
                         {
-                            item.Quality += 1;
-
-                            if (false)
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-                            }
                         }
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
+                        else
                         {
-                            if (item.Name == "Sulfuras, Hand of Ragnaros")
-                            {
-                            }
-                            else
-                            {
-                                item.Quality -= 1;
-                            }
+                            item.Quality -= 1;
                         }
                     }
 
@@ -178,21 +97,14 @@ public class GildedRose
 
                     if (item.SellIn < 0)
                     {
-                        if (false)
+                        if (item.Quality > 0)
                         {
-                            item.Quality -= item.Quality;
-                        }
-                        else
-                        {
-                            if (item.Quality > 0)
+                            if (item.Name == "Sulfuras, Hand of Ragnaros")
                             {
-                                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                                {
-                                }
-                                else
-                                {
-                                    item.Quality -= 1;
-                                }
+                            }
+                            else
+                            {
+                                item.Quality -= 1;
                             }
                         }
                     }
