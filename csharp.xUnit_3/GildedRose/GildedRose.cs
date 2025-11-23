092df83909a82709e91ffea7946c775ff719d107
James Woods <jmwoodsjr@gmail.com>
2025-11-22 22:11:49 -0500

    Extracted logic inside of if check on BackstagePasses to method UpdateBackstagePasses

--- csharp.xUnit_3/GildedRose/GildedRose.cs
+++ csharp.xUnit_3/GildedRose/GildedRose.cs
@@ -21,33 +21,7 @@ public class GildedRose
             }
             else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Quality < 50)
-                {
-                    item.Quality += 1;
-
-                    if (item.SellIn < 11)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
-                        }
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
-                        }
-                    }
-                }
-
-                item.SellIn -= 1;
-
-                if (item.SellIn < 0)
-                {
-                    item.Quality -= item.Quality;
-                }
+                UpdateBackstagePasses(item);
             }
             else if (item.Name == "Aged Brie")
             {
@@ -83,6 +57,37 @@ public class GildedRose
                     }
                 }
             }
+        }
+    }
+
+    private void UpdateBackstagePasses(Item item)
+    {
+        if (item.Quality < 50)
+        {
+            item.Quality += 1;
+
+            if (item.SellIn < 11)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+                }
+            }
+
+            if (item.SellIn < 6)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+                }
+            }
+        }
+
+        item.SellIn -= 1;
+
+        if (item.SellIn < 0)
+        {
+            item.Quality -= item.Quality;
         }
     }
 
