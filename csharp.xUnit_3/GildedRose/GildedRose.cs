20d2aab42f6ccb777a4240e5c5dadea83c5da553
James Woods <jmwoodsjr@gmail.com>
2025-11-22 21:38:59 -0500

    Removed all unreachable code

--- csharp.xUnit_3/GildedRose/GildedRose.cs
+++ csharp.xUnit_3/GildedRose/GildedRose.cs
@@ -23,77 +23,10 @@ public class GildedRose
                     if (item.Quality < 50)
                     {
                         item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
-
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name == "Sulfuras, Hand of Ragnaros")
-                            {
-                            }
-                            else
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
                     }
                 }
 
-                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                {
-                }
-                else
-                {
-                    item.SellIn -= 1;
-                }
+                item.SellIn -= 1;
 
                 if (item.SellIn < 0)
                 {
@@ -104,31 +37,11 @@ public class GildedRose
                             item.Quality += 1;
                         }
                     }
-                    else
-                    {
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            item.Quality -= item.Quality;
-                        }
-                        else
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                                {
-                                }
-                                else
-                                {
-                                    item.Quality -= 1;
-                                }
-                            }
-                        }
-                    }
                 }
             }
             else
             {
-                if (false)
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (item.Quality < 50)
                     {
@@ -156,43 +69,14 @@ public class GildedRose
                 }
                 else
                 {
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Quality > 0)
                     {
-                        if (item.Quality < 50)
+                        if (item.Name == "Sulfuras, Hand of Ragnaros")
                         {
-                            item.Quality += 1;
-
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality += 1;
-                                    }
-                                }
-                            }
                         }
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
+                        else
                         {
-                            if (item.Name == "Sulfuras, Hand of Ragnaros")
-                            {
-                            }
-                            else
-                            {
-                                item.Quality -= 1;
-                            }
+                            item.Quality -= 1;
                         }
                     }
                 }
@@ -207,30 +91,20 @@ public class GildedRose
 
                 if (item.SellIn < 0)
                 {
-                    if (false)
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
-                        }
+                        item.Quality -= item.Quality;
                     }
                     else
                     {
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality > 0)
                         {
-                            item.Quality -= item.Quality;
-                        }
-                        else
-                        {
-                            if (item.Quality > 0)
+                            if (item.Name == "Sulfuras, Hand of Ragnaros")
                             {
-                                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                                {
-                                }
-                                else
-                                {
-                                    item.Quality -= 1;
-                                }
+                            }
+                            else
+                            {
+                                item.Quality -= 1;
                             }
                         }
                     }
