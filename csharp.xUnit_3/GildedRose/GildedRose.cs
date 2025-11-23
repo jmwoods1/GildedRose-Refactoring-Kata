c10b8b4cc8ce6eb02fae53a3ea14cb6bd48edc38
James Woods <jmwoodsjr@gmail.com>
2025-11-22 21:30:12 -0500

    Extracted all if/else logic to function named Pants

--- csharp.xUnit_3/GildedRose/GildedRose.cs
+++ csharp.xUnit_3/GildedRose/GildedRose.cs
@@ -16,7 +16,41 @@ public class GildedRose
         foreach (var item in Items)
         {
             var isAgedBrie = item.Name == "Aged Brie";
-            if (isAgedBrie)
+            Pants(isAgedBrie, item);
+        }
+    }
+
+    private void Pants(bool isAgedBrie, Item item)
+    {
+        if (isAgedBrie)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn < 11)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality += 1;
+                        }
+                    }
+
+                    if (item.SellIn < 6)
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality += 1;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (item.Quality < 50)
                 {
@@ -44,77 +78,48 @@ public class GildedRose
             }
             else
             {
+                if (item.Quality > 0)
+                {
+                    if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    {
+                    }
+                    else
+                    {
+                        item.Quality -= 1;
+                    }
+                }
+            }
+        }
+
+        if (item.Name == "Sulfuras, Hand of Ragnaros")
+        {
+        }
+        else
+        {
+            item.SellIn -= 1;
+        }
+
+        if (item.SellIn < 0)
+        {
+            if (isAgedBrie)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+                }
+            }
+            else
+            {
                 if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-                        }
-                    }
+                    item.Quality -= item.Quality;
                 }
                 else
                 {
                     if (item.Quality > 0)
                     {
-                        if (item.Name == "Sulfuras, Hand of Ragnaros")
-                        {
-                        }
-                        else
-                        {
-                            item.Quality -= 1;
-                        }
-                    }
-                }
-            }
-
-            if (item.Name == "Sulfuras, Hand of Ragnaros")
-            {
-            }
-            else
-            {
-                item.SellIn -= 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (isAgedBrie)
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
-                }
-                else
-                {
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        item.Quality -= item.Quality;
-                    }
-                    else
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name == "Sulfuras, Hand of Ragnaros") continue;
-                            item.Quality -= 1;
-                        }
+                        if (item.Name == "Sulfuras, Hand of Ragnaros") return;
+                        item.Quality -= 1;
                     }
                 }
             }
